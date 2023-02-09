2e5f2a8225a2cc47c5da2b6d537e9b5bddd29cc6
Akif-Imran <raiden3322@gmail.com>
2023-02-10 01:21:11 +0500

    fixed preftch display to students in context

--- src/screens/teacher/StudentList.tsx
+++ src/screens/teacher/StudentList.tsx
@@ -7,32 +7,27 @@ import {
   ActivityIndicator,
   RefreshControl,
   TouchableOpacity,
-} from "react-native";
-import React, { useEffect, useLayoutEffect, useState } from "react";
-import { _StudentCard } from "../../components";
-import { _Button } from "../../components/general";
-import { Button, IconButton } from "react-native-paper";
-import { colors, gStyles } from "../../theme";
-import globalStyles from "../../theme/globalStyles";
-import * as ImagePicker from "expo-image-picker";
-import { useImagesContext, useUserContext } from "../../contexts";
-import { useNavigation, useRoute } from "@react-navigation/native";
-import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
-import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
-import Ionicons from "@expo/vector-icons/Ionicons";
-import {
-  ApiStudentsByClass,
-  ChangeStatusCallbackType,
-  TeacherStackScreenProps,
-} from "../../types";
-import { api } from "../../helpers";
-import { useStudentContext } from "../../contexts/StudentListProvider";
-import { session } from "../../helpers/api";
+} from 'react-native';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
+import { _StudentCard } from '../../components';
+import { _Button } from '../../components/general';
+import { Button, IconButton } from 'react-native-paper';
+import { colors, gStyles } from '../../theme';
+import globalStyles from '../../theme/globalStyles';
+import * as ImagePicker from 'expo-image-picker';
+import { useImagesContext, useUserContext } from '../../contexts';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { ApiStudentsByClass, ChangeStatusCallbackType, TeacherStackScreenProps } from '../../types';
+import { api } from '../../helpers';
+import { useStudentContext } from '../../contexts/StudentListProvider';
+import { session } from '../../helpers/api';
 
 const StudentList = () => {
-  const navigation =
-    useNavigation<TeacherStackScreenProps<"StudentList">["navigation"]>();
-  const route = useRoute<TeacherStackScreenProps<"StudentList">["route"]>();
+  const navigation = useNavigation<TeacherStackScreenProps<'StudentList'>['navigation']>();
+  const route = useRoute<TeacherStackScreenProps<'StudentList'>['route']>();
   const { images, setImages } = useImagesContext();
   const [visible, setIsVisible] = useState(false);
   const [isFetching, setIsFetching] = useState<boolean>(true);
@@ -42,11 +37,9 @@ const StudentList = () => {
 
   const fetchStudents = async () => {
     setIsFetching(true);
-    console.log("fetching Students", students);
+    console.log('fetching Students', students);
     api
-      .get(
-        `/student/get-all-students/${route.params.class}?session=${session}&courseName=${route.params.course}`
-      )
+      .get(`/student/get-all-students/${route.params.class}?session=${session}&courseName=${route.params.course}`)
       .then((res) => {
         if (res.status === 200) {
           const resData: ApiStudentsByClass = res.data;
@@ -73,12 +66,9 @@ const StudentList = () => {
       });
       console.log(result);
       if (!result.cancelled) {
-        if ("selected" in result) {
+        if ('selected' in result) {
           setImages(result.selected);
-          ToastAndroid.show(
-            "Make sure all Images are selected!",
-            ToastAndroid.SHORT
-          );
+          ToastAndroid.show('Make sure all Images are selected!', ToastAndroid.SHORT);
         } else {
           setImages([result]);
         }
@@ -117,7 +107,7 @@ const StudentList = () => {
 
   const markAttendance = async () => {
     try {
-      ToastAndroid.show("Please wait...", ToastAndroid.LONG);
+      ToastAndroid.show('Please wait...', ToastAndroid.LONG);
       if (!user) return;
       const attendances = students.flatMap((value) => [
         {
@@ -133,29 +123,29 @@ const StudentList = () => {
         venue: route.params.venue,
         slot: route.params.slot,
         jsonDate: new Date().toJSON(),
-        Session: "Spring-2022",
+        Session: 'Spring-2022',
         attendances: attendances,
       };
       let formData = new FormData();
       Object.entries(obj).forEach((value) => {
         if (Array.isArray(value[1])) {
-          console.log("array", value[0]);
+          console.log('array', value[0]);
           let arr = [];
           for (let obj of value[1]) {
             arr.push(JSON.stringify(obj));
           }
-          formData.append("attendances", JSON.stringify(arr));
+          formData.append('attendances', JSON.stringify(arr));
         } else {
           formData.append(value[0], value[1]);
         }
       });
-      const response = await api.post("/attendance/mark-attendance", formData, {
+      const response = await api.post('/attendance/mark-attendance', formData, {
         headers: {
-          "Content-Type": "multipart/form-data",
+          'Content-Type': 'multipart/form-data',
         },
       });
       if (response.status === 200) {
-        ToastAndroid.show("Attendance Marked Successfully", ToastAndroid.LONG);
+        ToastAndroid.show('Attendance Marked Successfully', ToastAndroid.LONG);
         setIsRefreshing(true);
         fetchStudents();
       }
@@ -166,7 +156,7 @@ const StudentList = () => {
 
   const goToImageViewer = async () => {
     try {
-      ToastAndroid.show("Please wait...", ToastAndroid.LONG);
+      ToastAndroid.show('Please wait...', ToastAndroid.LONG);
       if (!user) return;
       const attendances = students.flatMap((value) => [
         {
@@ -181,10 +171,10 @@ const StudentList = () => {
         venue: route.params.venue,
         slot: route.params.slot,
         jsonDate: new Date().toJSON(),
-        Session: "Spring-2022",
+        Session: 'Spring-2022',
         attendances: attendances,
       };
-      navigation.navigate("ImageViewer", obj);
+      navigation.navigate('ImageViewer', obj);
     } catch (err) {
       console.error(err);
     }
@@ -194,15 +184,8 @@ const StudentList = () => {
     navigation.setOptions({
       headerRight: () => (
         <>
-          <TouchableOpacity
-            style={styles.iconContainer}
-            onPress={goToImageViewer}
-          >
-            <MaterialCommunityIcons
-              name="animation"
-              size={20}
-              color={colors.white}
-            />
+          <TouchableOpacity style={styles.iconContainer} onPress={goToImageViewer}>
+            <MaterialCommunityIcons name="animation" size={20} color={colors.white} />
           </TouchableOpacity>
           <TouchableOpacity style={styles.iconContainer} onPress={pickImage}>
             <Ionicons name="image" color={colors.white} size={23} />
@@ -210,15 +193,8 @@ const StudentList = () => {
           <TouchableOpacity style={styles.iconContainer} onPress={captureImage}>
             <FontAwesome5 name="camera" size={20} color={colors.white} />
           </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.iconContainer}
-            onPress={markAttendance}
-          >
-            <MaterialCommunityIcons
-              name="send"
-              size={20}
-              color={colors.white}
-            />
+          <TouchableOpacity style={styles.iconContainer} onPress={markAttendance}>
+            <MaterialCommunityIcons name="send" size={20} color={colors.white} />
           </TouchableOpacity>
         </>
       ),
@@ -259,30 +235,58 @@ const StudentList = () => {
           icon="send"
         />
       </View> */}
-      <FlatList
+      {isFetching ? (
+        <View style={styles.activityIndicatorContainer}>
+          <ActivityIndicator color={colors.primary} size="large" animating={isFetching} />
+        </View>
+      ) : (
+        <FlatList
+          data={students}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={styles.listContentContainer}
+          renderItem={({ item, index }) => <_StudentCard student={item} changeStatus={changeStatus} key={item.id} />}
+          ListEmptyComponent={() => (
+            <View>
+              {isFetching ? (
+                <View style={styles.activityIndicatorContainer}>
+                  <ActivityIndicator color={colors.primary} size="large" animating={isFetching} />
+                </View>
+              ) : (
+                <View style={styles.mainContainer}>
+                  <Text style={styles.title}>No Data Found...</Text>
+                </View>
+              )}
+            </View>
+          )}
+          refreshControl={
+            <RefreshControl
+              enabled={true}
+              refreshing={isRefreshing}
+              colors={[colors.primary]}
+              progressBackgroundColor={colors.white}
+              onRefresh={() => {
+                setIsRefreshing(true);
+                fetchStudents();
+              }}
+            />
+          }
+          keyExtractor={(_, index) => index.toString()}
+        />
+      )}
+      {/* <FlatList
         data={students}
         showsVerticalScrollIndicator={false}
         contentContainerStyle={styles.listContentContainer}
-        renderItem={({ item, index }) => (
-          <_StudentCard
-            student={item}
-            changeStatus={changeStatus}
-            key={item.id}
-          />
-        )}
+        renderItem={({ item, index }) => <_StudentCard student={item} changeStatus={changeStatus} key={item.id} />}
         ListEmptyComponent={() => (
           <View>
             {isFetching ? (
               <View style={styles.activityIndicatorContainer}>
-                <ActivityIndicator
-                  color={colors.primary}
-                  size="large"
-                  animating={isFetching}
-                />
+                <ActivityIndicator color={colors.primary} size="large" animating={isFetching} />
               </View>
             ) : (
               <View style={styles.mainContainer}>
-                <Text style={styles.title}>204 Response...</Text>
+                <Text style={styles.title}>No Data Found...</Text>
               </View>
             )}
           </View>
@@ -300,7 +304,7 @@ const StudentList = () => {
           />
         }
         keyExtractor={(_, index) => index.toString()}
-      />
+      /> */}
       {/* <View style={styles.cardsContainer}>
         flat list was contained here originally
       </View> */}
@@ -334,14 +338,14 @@ const styles = StyleSheet.create({
   },
   activityIndicatorContainer: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   title: {
     ...gStyles.cardTitleText,
-    textAlign: "center",
+    textAlign: 'center',
     color: colors.titleText,
-    fontFamily: "Visby-Medium",
+    fontFamily: 'Visby-Medium',
   },
   listContentContainer: {
     // flex: 1,
@@ -354,13 +358,13 @@ const styles = StyleSheet.create({
   },
   submitButton: {
     // display: "flex",
-    width: "98%",
-    position: "absolute",
+    width: '98%',
+    position: 'absolute',
     bottom: 0,
     marginTop: 4,
     paddingHorizontal: 2,
     paddingBottom: 4,
-    justifyContent: "center",
+    justifyContent: 'center',
     marginHorizontal: 4,
     borderRadius: 50,
   },
